3a59b5096dcc2d44ddff3989ddd5b0ac7b3fb5f4
branimiranastasov <branimir.anastassov@gmail.com>
2025-01-21 21:33:59 +0200

    commit

---
+++ PostgreSQL/13_Database Programmability - Exeercises/05_difficulty_level.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION fn_difficulty_level(
+	level INT
+) RETURNS VARCHAR(50)
+AS
+$$
+DECLARE difficulty_level VARCHAR(50);
+BEGIN
+	IF level <= 40 THEN
+		difficulty_level := 'Normal Difficulty';
+	ELSIF level BETWEEN 41 AND 60 THEN
+		difficulty_level := 'Nightmare Difficulty';
+	ELSE
+		difficulty_level := 'Hell Difficulty';
+	END IF;
+	RETURN difficulty_level;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT
+	u.user_id,
+	u.level,
+	u.cash,
+	fn_difficulty_level(u.level)
+FROM
+	users_games AS u
+ORDER BY
+	u.user_id ASC;
