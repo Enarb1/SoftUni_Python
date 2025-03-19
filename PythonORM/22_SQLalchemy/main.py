42949141bf2e38f2ae897dc0e549c989508d2c50
branimiranastasov <branimir.anastassov@gmail.com>
2025-03-19 16:14:09 +0200

    commit

--- PythonORM/22_SQLalchemy/main.py
+++ PythonORM/22_SQLalchemy/main.py
@@ -1,14 +1,13 @@
+import random
+
 from sqlalchemy.orm import sessionmaker
-from models import User, engine
+from models import User, engine, City
 
 Session = sessionmaker(bind=engine)
 
 with Session() as session:
-    user = User(
-        first_name="John",
-        last_name="Smith",
-        age=36
-    )
-
-    session.add(user)
+    users = session.query(User).all()
+    for u in users:
+        u.city_id = random.randint(1,5)
     session.commit()
+
--- PythonORM/22_SQLalchemy/models.py
+++ PythonORM/22_SQLalchemy/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Relationship
 
 CONNECTION_STRING = 'postgresql+psycopg2://postgres:postgres@localhost:5432/orm_22'
 engine = create_engine(CONNECTION_STRING)
@@ -13,3 +14,13 @@ class User(Base):
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=False)
     age = Column(Integer, nullable=False)
+    city_id = Column(Integer, ForeignKey('cities.id'), default=1)
+    city = Relationship('City', backref='users')
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+
+Base.metadata.create_all(engine)
